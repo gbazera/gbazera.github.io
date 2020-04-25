48cc28b37d598bc5dabff1b8d6fc39ffc2552134
Lizard <giorgigamer27@gmail.com>
2020-04-25 19:02:41 +0400

    Update1

---
+++ app.js
@@ -0,0 +1,12 @@
+function changeText(){
+    const type = document.querySelector('.type');
+
+    const keywords = ['Web Developer', 'Front-End Developer', 'UI/UX Designer', 'Game Developer'];
+    const randomKeyword = Math.floor(Math.random() * keywords.length);
+
+    type.innerHTML = keywords[randomKeyword];
+
+    setTimeout(changeText, 3000);
+}
+
+changeText();
