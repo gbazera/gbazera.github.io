48022809534e8ac9c8e6e3cc9ad378e3db7e288c
Lizard <giorgigamer27@gmail.com>
2020-04-25 22:36:35 +0400

    Update4

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lizard</title>
+    <title>Lizard - Home</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700&display=swap">
     <link rel="stylesheet" href="style.css">
@@ -19,17 +19,17 @@
         </button>
 
         <ul class="nav-links">
-            <li><a href="#"><i class="bx bxs-home"></i> HOME</a></li>
-            <li><a href="#"><i class="bx bxs-user"></i> ABOUT</a></li>
-            <li><a href="#"><i class="bx bxs-book-content"></i> PORTFOLIO</a></li>
+            <li><a href="./index.html"><i class="bx bxs-home"></i> HOME</a></li>
+            <li><a href="./about.html"><i class="bx bxs-user"></i> ABOUT</a></li>
+            <li><a href="./portfolio.html"><i class="bx bxs-book-content"></i> PORTFOLIO</a></li>
         </ul>
     </nav>
     
     <div class="bg"></div>
-    <p class="title">I am <span class="type">Web Developer</span></p>
+    <p class="title">Welcome!<br> I am <span class="type">Web Developer</span></p>
 
     <footer>
-        <p class="copyright"><i class="bx bx-copyright"></i> Copyright <strong>lizardofc.github.io</strong></p>
+        <p class="copyright"><i class="bx bx-copyright"></i> Copyright <strong>lizardofc.ga</strong></p>
     </footer>
 
     <script src="./app.js"></script>
