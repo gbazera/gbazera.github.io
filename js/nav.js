7ca7257ab856b9e285a82ca7f0865504a709decb
gbazera <bazerashvili.gio27@gmail.com>
2022-05-15 15:18:51 +0400

    Fresh start

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.nav-links');
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', ()=>{
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('burger-active');
-})
